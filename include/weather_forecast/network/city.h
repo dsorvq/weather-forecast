812f372c8640053218885f42574fd85dd523f780
dsorvq <dsorvq@gmail.com>
2025-04-23 18:04:05 +0300

    Add TODO

--- include/weather_forecast/network/city.h
+++ include/weather_forecast/network/city.h
@@ -8,5 +8,29 @@ namespace weather_forecast {
     float longitude{};
   };
 
+  // TOOD: implements this
+  /*
+  struct Location {
+    std::string city_name;
+    Coordinates coordinates;
+  };
+
+  class CityCoordinateProvider {
+   public:
+    explicit CityCoordinateProvider(std::string api_key);
+
+    bool FetchCoordinates();
+    bool FetchCoordinates(const std::string& city_name);
+
+    Coordinates GetCityCoordinates();
+
+    bool IsOk();
+
+   private:
+    bool is_ok_{false};
+    Location location_;
+  };
+  */
+
   std::optional<Coordinates> GetCityCoordinates(const std::string& city_name);
 }
